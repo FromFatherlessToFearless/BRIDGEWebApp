ecef8f7b70f782be9fcdecd661515f85f2d515fe
shafferk83 <shafferk83@gmail.com>
2022-10-16 12:47:10 -0400

    take attendance

---
+++ Data/ViewModels/ParticipantAttendanceListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BRIDGEWebApp.Data.ViewModels
+{
+    public class ParticipantAttendanceListViewModel
+    {
+        public int AttendanceId { get; set; }
+        public DateTime AttendanceDate { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public int CohortId { get; set; }
+        public string CohortName { get; set; }
+        public List<ParticipantAttendanceViewModel> ParticipantAttendance { get; set; }
+        public ParticipantAttendanceListViewModel()
+        {
+            ParticipantAttendance = new List<ParticipantAttendanceViewModel>();
+        }
+
+    }
+}
---
+++ Data/ViewModels/ParticipantAttendanceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BRIDGEWebApp.Data.ViewModels
+{
+    public class ParticipantAttendanceViewModel
+    {
+        public int? ParticipantAttendanceId { get; set; }
+
+        public int ParticipantId { get; set; }
+        [DisplayName("In Attendance")]
+        public bool InAttendance { get; set; }
+        [StringLength(100)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+    }
+}
